fcc062943fe2fd0ed709ecb232681c2b97bd078b
EnChiSu <r07741074@ntu.edu.tw>
2022-10-30 21:35:35 +0800

    implemnet main page

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <ListComponent/>
+    </div>
+</template>
+<script>
+import ListComponent from './components/ListComponent.vue'
+export default {
+    name: 'App',
+    components: {
+        ListComponent
+    }
+}
+</script>
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+html, body {
+    background-color: #D3D3D3;
+}
+</style>
